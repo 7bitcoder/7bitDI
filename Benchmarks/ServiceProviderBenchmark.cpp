a6ee5ec3dfc824b6e53300da0beb0ec0072d6e36
7bitcoder <Sylwo1003@gmail.com>
2023-12-16 20:37:54 +0100

    extend benchmark

--- Benchmarks/ServiceProviderBenchmark.cpp
+++ Benchmarks/ServiceProviderBenchmark.cpp
@@ -1,9 +1,7 @@
-#include <SevenBit/DI.hpp>
 #include <benchmark/benchmark.h>
 
-class Test
-{
-};
+#include "Classes.hpp"
+#include <SevenBit/DI.hpp>
 
 class ServiceProviderBenchmark : public benchmark::Fixture
 {
@@ -14,22 +12,171 @@ class ServiceProviderBenchmark : public benchmark::Fixture
 
     void TearDown(const ::benchmark::State &state) {}
 
-    sb::di::ServiceProvider make() { return sb::di::ServiceCollection().addSingleton<Test>().buildServiceProvider(); }
+    sb::di::ServiceProvider make()
+    {
+        return sb::di::ServiceCollection()
+            .addSingleton<ITestComplexClass1, TestComplexClass1>()
+            .addSingleton<ITestComplexClass1, TestComplexClass12>()
+            .addSingleton<ITestComplexClass1, TestComplexClass13>()
+            .addSingleton<ITestComplexClass2, TestComplexClass2>()
+            .addTransient<ITestComplexClass3, TestComplexClass3>()
+            .addTransient<TestComplexClass3>()
+            .addScoped<ITestComplexClass4, TestComplexClass4>()
+            .addScoped<ITestComplexClass5, TestComplexClass5>()
+            .addScoped<ITestComplexClass6, TestComplexClass6>()
+            .buildServiceProvider();
+    }
 };
 
-BENCHMARK_F(ServiceProviderBenchmark, GetService)(benchmark::State &st)
+BENCHMARK_F(ServiceProviderBenchmark, GetServiceSimple)(benchmark::State &st)
 {
     for (auto _ : st)
     {
-        _provider.getService<Test>();
+        _provider.getService<ITestComplexClass1>();
     }
 }
 
-BENCHMARK_F(ServiceProviderBenchmark, TryGetService)(benchmark::State &st)
+BENCHMARK_F(ServiceProviderBenchmark, GetServiceOneRef)(benchmark::State &st)
 {
     for (auto _ : st)
     {
-        _provider.tryGetService<Test>();
+        _provider.getService<ITestComplexClass2>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServiceUniqueRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getService<ITestComplexClass4>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServiceManyRefs)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getService<ITestComplexClass5>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServiceVecRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getService<ITestComplexClass6>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, CreateService)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.createService<ITestComplexClass3>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, CreateServicesInPlace)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.createServiceInPlace<TestComplexClass3>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceSimple)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass1>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceOneRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass2>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceUnique)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass3>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceUniqueRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass4>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceManyRefs)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass5>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, TryGetServiceVecRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.tryGetService<ITestComplexClass6>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesSimple)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass1>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesOneRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass2>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesUnique)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass3>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesUniqueRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass4>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesManyRefs)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass5>();
+    }
+}
+
+BENCHMARK_F(ServiceProviderBenchmark, GetServicesVecRef)(benchmark::State &st)
+{
+    for (auto _ : st)
+    {
+        _provider.getServices<ITestComplexClass6>();
     }
 }
 
