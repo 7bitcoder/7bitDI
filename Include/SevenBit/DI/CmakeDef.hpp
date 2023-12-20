009e4e6d3c24bbe6cea0052d3283f9e096eab33d
7bitcoder <Sylwo1003@gmail.com>
2023-12-20 16:05:36 +0100

    update cmakedef

---
+++ Include/SevenBit/DI/CmakeDef.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _7BIT_DI_SHARED_LIB
+/* #undef _7BIT_DI_SHARED_LIB */
+#endif
+
+#ifndef _7BIT_DI_STATIC_LIB
+/* #undef _7BIT_DI_STATIC_LIB */
+#endif
+
+#ifndef _7BIT_DI_HEADER_ONLY_LIB
+#define _7BIT_DI_HEADER_ONLY_LIB
+#endif
+
+#define _7BIT_DI_VERSION_MAJOR 2
+/* #undef _7BIT_DI_VERSION_MINOR */
+/* #undef _7BIT_DI_VERSION_PATCH */
