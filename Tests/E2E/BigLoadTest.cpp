09555fdb34a8ec18501113a7882245dd9fce1ad0
7bitcoder <Sylwo1003@gmail.com>
2024-03-16 20:45:57 +0100

    reduce big test size

--- Tests/E2E/BigLoadTest.cpp
+++ Tests/E2E/BigLoadTest.cpp
@@ -5,6 +5,7 @@ int main()
     constexpr size_t Size = 1000;
     sb::di::ServiceCollection services;
     addLegions<2000, Size>(services);
+    addLegions<4000, Size>(services);
 
     sb::di::ServiceProviderOptions options;
     options.prebuildSingletons = true;
