8ae0e71f8a3a5414457c4827027c8ed51e4fc3b5
7bitcoder <sylwester.dawida@gmail.com>
2024-07-31 16:09:30 +0200

    update Exceptions.hpp

--- Include/SevenBit/DI/Impl/Exceptions.hpp
+++ Include/SevenBit/DI/Impl/Exceptions.hpp
@@ -13,22 +13,22 @@ namespace sb::di
 
     INLINE NullPointerException::NullPointerException(const std::string &why) : InjectorException{why} {}
 
-    INLINE ServiceRegisterException::ServiceRegisterException(const TypeId typeId, const std::string &reason)
+    INLINE ServiceRegisterException::ServiceRegisterException(const TypeId typeId, std::string_view reason)
         : InjectorException{details::String::fmt("Cannot register service '{}', reason: {}.", typeId.name(), reason)}
     {
     }
 
-    INLINE ServiceNotFoundException::ServiceNotFoundException(const TypeId typeId, const std::string &reason)
+    INLINE ServiceNotFoundException::ServiceNotFoundException(const TypeId typeId, std::string_view reason)
         : InjectorException{details::String::fmt("Service '{}' was not found, reason: {}.", typeId.name(), reason)}
     {
     }
 
-    INLINE CannotMoveOutServiceException::CannotMoveOutServiceException(const TypeId typeId, const std::string &reason)
+    INLINE CannotMoveOutServiceException::CannotMoveOutServiceException(const TypeId typeId, std::string_view reason)
         : InjectorException{details::String::fmt("Cannot move out service '{}', reason: {}.", typeId.name(), reason)}
     {
     }
 
-    INLINE CannotReleaseServiceException::CannotReleaseServiceException(const TypeId typeId, const std::string &reason)
+    INLINE CannotReleaseServiceException::CannotReleaseServiceException(const TypeId typeId, std::string_view reason)
         : InjectorException{
               details::String::fmt("Cannot release ownership of service '{}', reason: {}.", typeId.name(), reason)}
     {
